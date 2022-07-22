f5d2294613e240fd12ee495f0d38c740bb0088d1
Corey Lundy <clundycap@gmail.com>
2022-07-22 16:26:54 -0500

    Fixed deprecated function use

--- DeadGunner/lua/radar_1.onEnter.lua
+++ DeadGunner/lua/radar_1.onEnter.lua
@@ -3,7 +3,7 @@ if SZD ~= nil and bootTimer > 0 then
     if not inSZ and SZD*0.000005 > radarBuffer then
         system.stopSound()
         system.playSound('contact.mp3')
-        if vec3(core_1.getVelocity()):len() * 3.6 > 50000 then
+        if vec3(construct.getVelocity()):len() * 3.6 > 50000 then
             warpScan[id] = cPos
         else
             unknownRadar[id] = cPos
