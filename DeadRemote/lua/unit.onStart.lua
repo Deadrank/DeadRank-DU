1cc55047cd7e91b057ad495cc8c1a3df26b6228d
Corey Lundy <clundycap@gmail.com>
2022-08-20 21:53:58 -0500

    Update remote version variable

--- DeadRemote/lua/unit.onStart.lua
+++ DeadRemote/lua/unit.onStart.lua
@@ -14,7 +14,7 @@ table.insert(predefinedTags,'freight')
 showAlerts = false
 
 ---------------------------------------
-hudVersion = 'v3.1.0'
+hudVersion = 'v3.1.1'
 minimalWidgets = false --export
 validatePilot = false --export
 useDB = true --export
